80e643329e387794ca267835a9efd51fa619c7e6
ValeriaGallegoR <valeriagallego20@gmail.com>
2025-04-09 15:08:40 -0400

    paintRequiredCalculator.ts: Negative values validation is added in the function.

--- src/paintRequiredCalculator/paintRequiredCalculator.ts
+++ src/paintRequiredCalculator/paintRequiredCalculator.ts
@@ -1,5 +1,9 @@
 export default function paintRequiredCalculator(length: number, height: number, copies: number): number {
 
+  if (length <= 0 || height <= 0 || copies < 0) {
+        throw new Error('Length, height, and copies must be positive values.');
+      }
+        
   const rectangleArea = length * height;
   const triangleArea = (length / 3) * height * 0.5;
   const totalAreaPerCopy = rectangleArea + 2 * triangleArea;
